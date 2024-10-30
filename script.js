e62057cb41e7f192450ec44f5a958d69d803c75a
larsen122 <maria.pinton@escola.pr.gov.br>
2024-10-30 16:30:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 //Elementos do Dom
-const musicForm=document.getElementByld('musicForm');
-const musicTableBody=document.querySelector('#musicTable tbody');
-const exportBtn=document.getElementByld('exportBtn');
+const musicForm=document.getElementByld('musicForm)');
+const music TableBody=document.querySelector('#musicTable tbody');
+const exporBtn=docoment.getElementByld('exportBtn');
 
 //Função para adicionar uma linha na tabela
-musicForm.addEvenListener('submit',function(event){
-    event.preventDelault();
-    const numero=document.getEElementByld('numero').value;
-    const musica=document.getElementByld('musica').value;
-const genero=document.getElementByld('cantor').value;
-const autoria=document.getElementByld('autoria').value;
+// musicForm.addEvenListener('submit',function(event){event.preventDefault();
+    event.preventDefault();  
 
-const newRow=document.createElement('tr');
-newRow.innerHTML=`
-<td>${numero}</td>
-<td>${musica}</td>
-<td>${genero}</td>
-<td>${cantor}</td>
-<td>${autoria}</td>
-´;
+    const numero=document.getElementBydl('numero') value;
+    const musica=document.getElementByld('musica')value;
+    const genero=document.getElementByld('genero')value;
+    const cantor=document.getElementByld('cantor')value;
+    const autoria=document.getElementByld('autoria')value;
 
-music TableBody.appendChild(newRow);
+    const newRow=document.createElement('tr');
+    newRow.innerHTML=´
+       <td>${numero}</td>
+       <td>${musica}</td>
+       <td>${genero}</td>
+       <td>${cantor}</td>
+       <td>${autoria}</td>  
+    ´;
 
-//limpar
+      musicTableBody.appendChild(newRow)
+
+      //Limpar o formulário
+      musicForm.reset();
+ });
+
+ //Função para exportar a tabela como planilha dinÂmica
+ export
